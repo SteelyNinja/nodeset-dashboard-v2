c0630f495a9866db861f11a86c2ec405c5d37e3b
SteelyNinja <gary@garypower.com>
2025-08-10 22:11:03 +0000

    Add OutagesTab import and routing to App component
    Include OutagesTab component and add outages case to tab routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import DistributionTab from './components/tabs/DistributionTab';
 import ConcentrationTab from './components/tabs/ConcentrationTab';
 import OperatorsTab from './components/tabs/OperatorsTab';
 import PerformanceTab from './components/tabs/PerformanceTab';
+import OutagesTab from './components/tabs/OutagesTab';
 import ProposalsTab from './components/tabs/ProposalsTab';
 import SyncCommitteeTab from './components/tabs/SyncCommitteeTab';
 import ExitAnalysisTab from './components/tabs/ExitAnalysisTab';
@@ -126,6 +127,8 @@ function MainDashboard() {
         return <OperatorsTab />;
       case 'performance':
         return <PerformanceTab />;
+      case 'outages':
+        return <OutagesTab />;
       case 'proposals':
         return <ProposalsTab />;
       case 'sync-committee':
